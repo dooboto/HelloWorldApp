68fa82f13628fba91e01c445c279b63c585d06cb
Drew DeBruyne <ddebruyne@outlook.com>
2025-03-18 19:41:15 -0700

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Drew DeBruyne on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
